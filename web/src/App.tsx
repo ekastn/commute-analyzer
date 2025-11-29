e408cc5b3a5982409e330aa29ea53c74b59005d5
ekastn <118880703+ekastn@users.noreply.github.com>
2025-11-29 12:16:34 +0700

    feat: setup frontend

---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center p-24">
+            <h1 className="text-3xl font-bold underline"> Hello world! </h1>
+        </main>
+    );
+};
+
+export default App;
